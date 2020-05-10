3dd81ddcd3a85d5105e5303cc2aae452edc27892
jhamPac <cloud@kevia.me>
2020-05-09 21:38:38 -0700

    creating a new node

--- go.mod
+++ go.mod
@@ -8,11 +8,16 @@ require (
 	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
 	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/gorilla/mux v1.7.4
+	github.com/ipfs/go-ipfs-config v0.5.3 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/kr/pretty v0.2.0 // indirect
+	github.com/libp2p/go-libp2p-core v0.5.3 // indirect
+	github.com/multiformats/go-multibase v0.0.2 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
 	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421
+	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.5 // indirect
 )
--- ipfs.go
+++ ipfs.go
@@ -1,7 +1,65 @@
 package kudurru
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"path/filepath"
 
-func spawnDefault(ctx context.Context) (icore.CoreAPI, err) {
+	config "github.com/ipfs/go-ipfs-config"
+	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/coreapi"
+	"github.com/ipfs/go-ipfs/core/node/libp2p"
+	"github.com/ipfs/go-ipfs/plugin/loader"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+	icore "github.com/ipfs/interface-go-ipfs-core"
+)
 
+func setupPlugins(externalPluginsPath string) error {
+	plugins, err := loader.NewPluginLoader(filepath.Join(externalPluginsPath, "plugins"))
+	if err != nil {
+		return fmt.Errorf("error loading plugins: %s", err)
+	}
+
+	if err := plugins.Initialize(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	if err := plugins.Inject(); err != nil {
+		return fmt.Errorf("error injecting plugins: %s", err)
+	}
+
+	return nil
+}
+
+func spawnDefault(ctx context.Context) (icore.CoreAPI, error) {
+	dPath, err := config.PathRoot()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := setupPlugins(dPath); err != nil {
+		return nil, err
+	}
+
+	return createNode(ctx, dPath)
+}
+
+func createNode(ctx context.Context, repoPath string) (icore.CoreAPI, error) {
+	repo, err := fsrepo.Open(repoPath)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeOptions := &core.BuildCfg{
+		Online:  true,
+		Routing: libp2p.DHTOption,
+		Repo:    repo,
+	}
+
+	node, err := core.NewNode(ctx, nodeOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return coreapi.NewCoreAPI(node)
 }
