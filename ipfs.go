841fd3b7ceeeb6f0dc7b186d28066eb958b9e9fa
jhamPac <cloud@kevia.me>
2020-05-09 21:02:25 -0700

    first function for ipfs stuff

--- ipfs.go
+++ ipfs.go
@@ -1,1 +1,25 @@
 package kudurru
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/ipfs/go-ipfs/plugin/loader"
+)
+
+func setupPlugins(externalPluginsPath string) error {
+	plugins, err := loader.NewPluginLoader(filepath.Join(externalPluginsPath, "plugins"))
+	if err != nil {
+		return fmt.Errorf("error loading plugins: %s", err)
+	}
+
+	if err := plugins.Initialize(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	if err := plugins.Inject(); err != nil {
+		return fmt.Errorf("error injecting plugins: %s", err)
+	}
+
+	return nil
+}
